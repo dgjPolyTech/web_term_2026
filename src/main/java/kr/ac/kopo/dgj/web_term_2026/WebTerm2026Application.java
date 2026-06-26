75918e2633b294df99e37e4f84b68ee002d70315
dgjPolyTech <dgjpolytech@gmail.com>
2026-06-26 16:40:45 +0900

    initial commit

---
+++ src/main/java/kr/ac/kopo/dgj/web_term_2026/WebTerm2026Application.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.dgj.web_term_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTerm2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebTerm2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/dgj/web_term_2026/WebTerm2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.dgj.web_term_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTerm2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
